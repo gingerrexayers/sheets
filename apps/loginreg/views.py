512618776262fdea83cd70aa27992783c3cb7027
Mark Ayers <markthema3@gmail.com>
2016-09-29 10:21:07 -0700

    initial commit

---
+++ apps/loginreg/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import User
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+# Create your views here.
+
+def index(request):
+    return render(request, 'loginreg/index.html')
+
+def register(request):
+    if request.method=='POST':
+        result = User.manager.register(request.POST)
+
+        if result[0]:
+            request.session['id'] = result[1].id
+            return redirect(reverse('login:registersuccess'))
+        else:
+            for e in result[1]:
+                messages.error(request, e)
+            return redirect(reverse('login:index'))
+
+def login(request):
+    if request.method=='POST':
+        result = User.manager.login(request.POST)
+        if result[0]:
+            request.session['id'] = result[1].id
+            return redirect(reverse('login:loginsuccess'))
+        else:
+            for e in result[1]:
+                messages.error(request, e)
+            return redirect(reverse('login:index'))
+
+def logout(request):
+    request.session.pop('id')
+    return redirect(reverse('login:index'))
+
+def success(request):
+    return redirect(reverse('sheets:index'))
