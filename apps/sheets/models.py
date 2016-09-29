12b1d7750d52a3a491421b2457355618a22e159a
Mark Ayers <markthema3@gmail.com>
2016-09-29 10:32:06 -0700

    add char model

--- apps/sheets/models.py
+++ apps/sheets/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from ..loginreg.models import User
 # Create your models here.
+
+class Character(models.model):
+    ### META ###
+    user = models.ForeignKey(User)
+    ### INFO ###
+    name = models.CharField(max_length=255)
+    alignment = models.CharField(max_length=255)
+    race = models.CharField(max_length=255)
+    char_class = models.CharField(max_length=255)
+    level = models.IntegerField(default=1)
+    exp = models.IntegerField(default=0)
+    ### STATS ###
+	curr_hp = models.IntegerField()
+    max_hp = models.IntegerField()
+    damage = models.IntegerField()
+    armor = models.IntegerField()
+    strength = models.IntegerField()
+    dexterity = models.IntegerField()
+    constitution = models.IntegerField()
+    intelligence = models.IntegerField()
+    wisdom = models.IntegerField()
+    charisma = models.IntegerField()
