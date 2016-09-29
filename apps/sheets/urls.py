439f40aa12ece93c3ff4ad7c67186554ed438959
Mark Ayers <markthema3@gmail.com>
2016-09-29 10:52:23 -0700

    generated form for char

--- apps/sheets/urls.py
+++ apps/sheets/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     url(r'^', views.index, name='index'),
     url(r'^add$', views.add, name='add'),
-    url(r'^create$', views.create, name='create')
+    url(r'^create$', views.create, name='create'),
+    url(r'^(?P<id>\d+)$', views.show, name='show'),
 ]
--- apps/sheets/views.py
+++ apps/sheets/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ..loginreg.models import User
+from .models import Character
+from .forms import CharacterForm
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    u = User.get(id=request.session['id'])
+    c = Character.filter(user=u)
+    context = {
+        'user': u,
+        'characters': c
+    }
+    return render(request, 'sheets/index.html', context)
+
+def add(request):
+    return render(request, 'sheets/add.html')
+
+def create(request):
+    if request.method == 'POST':
+        bound_form = CharacterForm(request.POST)
+        if bound_form.is_valid():
+            c = Character.manager.create(bound_form)
+            # TODO: redirect to show page for Character
+            return redirect(reverse('sheets:index'))
+        else:
+            for e in bound_form.errors:
+                messages.error(request, e)
+    return redirect(reverse('sheets:add'))
+
+def show(request):
+    
